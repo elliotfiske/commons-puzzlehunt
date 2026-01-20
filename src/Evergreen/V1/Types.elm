84d7402771fbf8a20e88766b3bd7661ebe9f24bd
Elliot Fiske <elliotfiske@gmail.com>
2026-01-19 19:12:20 -0800

    Preparing for v1

--- src/Evergreen/V1/Types.elm
+++ src/Evergreen/V1/Types.elm
@@ -3,17 +3,72 @@ module Evergreen.V1.Types exposing (..)
 import Effect.Browser
 import Effect.Browser.Navigation
 import Effect.Lamdera
+import SeqDict
 import Url
+
+
+type StashId
+    = Moonshine
+    | Whiskey
+    | Gin
+    | Bourbon
+    | Rum
+
+
+type Route
+    = IntroRoute
+    | HubRoute
+    | PaintingsRoute
+    | LedgerRoute
+    | StashRoute
+    | StashFoundRoute StashId
+    | TileRoute
+    | NotFoundRoute
+
+
+type alias StashProgress =
+    { moonshine : Bool
+    , whiskey : Bool
+    , gin : Bool
+    , bourbon : Bool
+    , rum : Bool
+    }
+
+
+type alias UserProgress =
+    { hasSeenIntro : Bool
+    , puzzle1Complete : Bool
+    , puzzle2Complete : Bool
+    , puzzle3Stashes : StashProgress
+    , puzzle3Complete : Bool
+    , puzzle4Complete : Bool
+    }
+
+
+type PuzzleId
+    = Puzzle1
+    | Puzzle2
+    | Puzzle3
+    | Puzzle4
+
+
+type AnswerResult
+    = NoAnswerYet
+    | Incorrect PuzzleId
+    | Correct PuzzleId String
 
 
 type alias FrontendModel =
     { key : Effect.Browser.Navigation.Key
-    , message : String
+    , route : Route
+    , userProgress : Maybe UserProgress
+    , puzzleInput : String
+    , lastAnswerResult : AnswerResult
     }
 
 
 type alias BackendModel =
-    { message : String
+    { userProgress : SeqDict.SeqDict Effect.Lamdera.SessionId UserProgress
     }
 
 
@@ -21,10 +76,16 @@ type FrontendMsg
     = UrlClicked Effect.Browser.UrlRequest
     | UrlChanged Url.Url
     | NoOpFrontendMsg
+    | PuzzleInputChanged String
+    | SubmitAnswer PuzzleId
+    | ClickedBegin
+    | NavigateTo String
 
 
 type ToBackend
-    = NoOpToBackend
+    = MarkIntroSeen
+    | SubmitPuzzleAnswer PuzzleId String
+    | MarkStashFound StashId
 
 
 type BackendMsg
@@ -34,4 +95,6 @@ type BackendMsg
 
 
 type ToFrontend
-    = NoOpToFrontend
+    = InitialState UserProgress
+    | PuzzleAnswerResult PuzzleId (Maybe String)
+    | StashMarked StashId
